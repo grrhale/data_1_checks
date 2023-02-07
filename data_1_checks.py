0b482e18583936edc2efcb69dc94b6c41ebf1119
rhale <raymond.hale0@gmail.com>
2023-02-07 17:16:29 -0500

    first commit, knowledge check 1 complete

---
+++ data_1_checks.py
@@ -0,0 +1,21 @@
+# List for knowledge check
+kclist = ["red", "green", "blue"]
+
+# Dictionary for knowledge check
+kcdict = {
+	"filetype":".JPG",
+	"created":"2012",
+	}
+
+# Tuple for knowledge check
+kctuple = ("7","9","6","8")
+
+
+# Printing from each of these, for the knowledge check
+print("Hello world!\n")
+print("Here's something from my list:")
+print(kclist[0])
+print("And my dictionary:")
+print(kcdict["filetype"])
+print("...and my tuple!")
+print(kctuple[0])
